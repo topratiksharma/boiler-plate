a66efb12b261741766787100738c4e517b7789f2
Pratik Sharma <pratiksharma@live.in>
2023-08-31 22:45:20 +0530

    initial design

--- boiler-plate-code/src/app/components/home/home.module.ts
+++ boiler-plate-code/src/app/components/home/home.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
